68adf3ceea37bdbf59519a45f3329a5aab749b54
kaheichan <53677691+kaheichanturtle@users.noreply.github.com>
2026-01-27 12:41:38 +1100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'globotcorp.tai.' + new Date().toISOString();
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
+  'https://fonts.googleapis.com/css2?family=Nunito:wght@400;700;900&display=swap',
+  'https://globot.neocities.org/t.ai/minimaltai.png',
+  'https://globot.neocities.org/t.ai/tai.png'
+];
+
+// Install Event
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+// Fetch Event
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      // Return cache if found, otherwise fetch from network
+      return response || fetch(event.request);
+    })
+  );
+});
